5f57c44ccadd0879c485270fb342fdde0fed7619
Martin <m4rtin.t@gmail.com>
2018-07-07 17:17:08 -0700

    Refactored Prefabs and scripts

--- Assets/Script/Manager/UIManager.cs
+++ Assets/Script/Manager/UIManager.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
 
     //Dependencies
     TimeManager m_TimeManager;
+    ScoreManager m_ScoreManager;
 
     [SerializeField]
     GameObject m_FadedBackgroundObject;
@@ -14,8 +15,10 @@ public class UIManager : MonoBehaviour
     GameObject m_GameOverObject;
     [SerializeField]
     GameObject m_PauseScreenObject;
-
-
+    [SerializeField]
+    GameObject m_ScoreTextObject;
+    [SerializeField]
+    GameObject m_GameTimeObject;
 
     #region Mono
     private void Awake()
@@ -27,11 +30,14 @@ public class UIManager : MonoBehaviour
     void InitManagers()
     {
         m_TimeManager = TimeManager.instance;
+        m_ScoreManager = ScoreManager.instance;
     }
 
     void InitDelegate()
     {
+        m_ScoreManager.thisDelegate += UpdateScore;
         m_TimeManager.CheckGameOverDelegate += ShowGameOverScreen;
+        m_TimeManager.UpdateTimerUIDelegate += UpdateTime;
     }
     #endregion
 
@@ -43,8 +49,14 @@ public class UIManager : MonoBehaviour
     }
     #endregion
 
-    #region GameOverPanel
+    #region Game Interface
+    void UpdateScore(){
+        m_ScoreTextObject.GetComponent<Text>().text = m_ScoreManager.GetScore().ToString() + "\n" + "x" + m_ScoreManager.IncrementScoreVariable();
+    }
 
+    void UpdateTime() {
+        m_GameTimeObject.GetComponent<Text>().text = m_TimeManager.GameTime();
+    }
     #endregion
 
     #region Pause Panels
