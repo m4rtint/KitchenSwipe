6ba1b9e61e822ada820e934c11200818f6a96a88
Martin <m4rtin.t@gmail.com>
2018-07-07 18:11:52 -0700

    Seperate movement from user input

---
+++ Assets/CenterIngredientMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterIngredientMovement : MonoBehaviour
+{
+    [SerializeField]
+    float m_SnapBackSpeed;
+
+    Vector3 m_StartPosition;
+    bool m_StartAnimation;
+
+    #region Mono
+    // Use this for initialization
+    void Awake()
+    {
+        m_StartPosition = transform.position;
+        m_StartAnimation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_StartAnimation)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, m_StartPosition, Time.deltaTime * m_SnapBackSpeed);
+            if (transform.position == m_StartPosition)
+            {
+                m_StartAnimation = false;
+            }
+        }
+    }
+    #endregion
+
+    #region Setter
+    public void StartAnimation()
+    {
+        m_StartAnimation = true;
+    }
+    #endregion
+
+    #region Movement
+    void Movement(Direction dir){
+        Vector3 position = transform.position;
+
+        if (dir == Direction.Up || dir == Direction.Down){
+            transform.position = new Vector3(position.x, Input.mousePosition.y);
+        } else {
+            
+        }
+
+        switch(dir){
+            case Direction.Up:
+            case Direction.Down:
+                transform.position = new Vector3(position.x, Input.mousePosition.y);
+                break;
+            case Direction.Left:
+                break;
+            case Direction.Right:
+                break;
+            default:
+                break;
+        }
+    }
+    #endregion
+
+}
--- Assets/Script/UserInput.cs
+++ Assets/Script/UserInput.cs
@@ -11,69 +11,75 @@ public enum Direction {
     Down
 }
 
-public class UserInput  : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+public class UserInput : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
 
-	public delegate void InputDelegate(Direction dir);
-	public InputDelegate thisDelegate;
+    public delegate void InputDelegate(Direction dir);
+    public InputDelegate thisDelegate;
 
-    Vector3 startPosition;
 
-	[SerializeField]
-	float m_DragDistance;
-
-    private void Awake()
-    {
-        startPosition = transform.position;
-    }
+    [SerializeField]
+    float m_DragDistance;
 
     Direction GetDragDirection(Vector3 dragVector)
-	{
-		float positiveX = Mathf.Abs(dragVector.x);
-		float positiveY = Mathf.Abs(dragVector.y);
-		Direction draggedDir;
-		if (positiveX > positiveY)
-		{
-			draggedDir = (dragVector.x > 0) ? Direction.Right : Direction.Left;
-		}
-		else
-		{
-			draggedDir = (dragVector.y > 0) ? Direction.Up : Direction.Down;
-		}
+    {
+        float positiveX = Mathf.Abs(dragVector.x);
+        float positiveY = Mathf.Abs(dragVector.y);
+        Direction draggedDir;
+        if (positiveX > positiveY)
+        {
+            draggedDir = (dragVector.x > 0) ? Direction.Right : Direction.Left;
+        }
+        else
+        {
+            draggedDir = (dragVector.y > 0) ? Direction.Up : Direction.Down;
+        }
 
-		#if UNITY_EDITOR
-		if (DebugManager.instance.C_Direction) {
-			Debug.Log (draggedDir);
-		}
-		#endif
-		return draggedDir;
-	}
-
-	bool Paused(){
-		return StateManager.instance.Paused ();
-	}
-
-
-	#region Events
-	//Do this when the user stops dragging this UI Element.
-	public void OnEndDrag(PointerEventData eventData)
-	{
-		
-		Vector2 displacement = eventData.position - eventData.pressPosition;
-		float distance = displacement.magnitude;
-		if (distance < m_DragDistance || Paused()) {
-			return;
-		}
-
-		Vector3 dragVectorDirection = displacement.normalized;
-		Direction dir = GetDragDirection(dragVectorDirection);
-		this.thisDelegate (dir);
-	}
-
-	public void OnBeginDrag(PointerEventData data) {}
-
-	public void OnDrag(PointerEventData data){
-        transform.position = Input.mousePosition;
+#if UNITY_EDITOR
+        if (DebugManager.instance.C_Direction)
+        {
+            Debug.Log(draggedDir);
+        }
+#endif
+        return draggedDir;
     }
-	#endregion
+
+    bool Paused()
+    {
+        return StateManager.instance.Paused();
+    }
+
+
+    #region Events
+    //Do this when the user stops dragging this UI Element.
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        Vector2 displacement = eventData.position - eventData.pressPosition;
+        float distance = displacement.magnitude;
+        if (distance < m_DragDistance || Paused())
+        {
+            GetComponent<CenterIngredientMovement>().StartAnimation();
+            return;
+        }
+
+        Vector3 dragVectorDirection = displacement.normalized;
+        Direction dir = GetDragDirection(dragVectorDirection);
+        this.thisDelegate(dir);
+    }
+
+    public void OnBeginDrag(PointerEventData data) { }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 displacement = eventData.position - eventData.pressPosition;
+        Vector3 dragVectorDirection = displacement.normalized;
+        Direction dir = GetDragDirection(dragVectorDirection);
+
+
+        transform.position = Input.mousePosition;
+        //Algorithm to only allow up down left right
+    }
+    #endregion
 }
 
