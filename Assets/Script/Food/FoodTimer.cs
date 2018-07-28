ee4bcdd0ddbb15eccb6d791b60653dda740a7b32
Martin Tsang <m4rtin.t@gmail.com>
2018-07-28 14:36:30 -0700

    Change timer with colors and background

--- Assets/Script/Food/FoodTimer.cs
+++ Assets/Script/Food/FoodTimer.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 public class FoodTimer : MonoBehaviour {
 
     float m_SecondsToComplete;
+    Color m_HundredPercent;
+    Color m_FourtyPercent;
+    Color m_TwentyPercent;
 
     //Dependencies
     FoodHolder m_FoodHolder;
@@ -18,6 +21,13 @@ public class FoodTimer : MonoBehaviour {
     private void Awake()
     {
         m_FoodHolder = GetComponent<FoodHolder>();
+    }
+
+    private void Start()
+    {
+        m_HundredPercent = GetConvertColor(109, 255, 0);
+        m_FourtyPercent = GetConvertColor(250,138,2);
+        m_TwentyPercent = GetConvertColor(255,3,0);
     }
     #endregion
 
@@ -45,25 +55,45 @@ public class FoodTimer : MonoBehaviour {
     {
         m_SecondsToComplete -= seconds;
     }
-    #endregion
-    
-    #region view
-    public void UpdateTimer(float seconds)
-    {
-        DecrementTimeBy(seconds);
-        UpdateTimerUI();
-        TimerAtZero();
-    }
 
-    void UpdateTimerUI()
+    float GetRatio()
     {
         float ratio = 0;
         if (FoodTime() > 0)
         {
             ratio = m_SecondsToComplete / FoodTime();
-        } 
-        m_TimerObject.GetComponent<RectTransform>().localScale = new Vector3(ratio, 1, 1);
+        }
+        return ratio;
+    }
+    #endregion
 
+    #region view
+    public void UpdateTimer(float seconds)
+    {
+        DecrementTimeBy(seconds);
+        UpdateTimerUI();
+        UpdateTimerColor();
+        TimerAtZero();
+    }
+
+    void UpdateTimerUI()
+    {
+        m_TimerObject.GetComponent<RectTransform>().localScale = new Vector3(GetRatio(), 1, 1);
+    }
+
+    void UpdateTimerColor()
+    {
+        float ratio = GetRatio();
+        if (ratio > 0.4)
+        {
+            m_TimerObject.GetComponent<Image>().color = m_HundredPercent;
+        } else if(ratio > 0.2)
+        {
+            m_TimerObject.GetComponent<Image>().color = m_FourtyPercent;
+        } else
+        {
+            m_TimerObject.GetComponent<Image>().color = m_TwentyPercent;
+        }
     }
 
     void TimerAtZero()
@@ -77,6 +107,14 @@ public class FoodTimer : MonoBehaviour {
     }
     #endregion
 
-
+    #region tools
+    Color GetConvertColor(float r, float g, float b)
+    {
+        float red = r / 255;
+        float green = g / 255;
+        float blue = b / 255;
+        return new Color(red, green, blue);
+    } 
+    #endregion
 
 }
