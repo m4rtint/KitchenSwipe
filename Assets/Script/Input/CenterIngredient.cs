b76bb23813cbe65fd6a02c08ab65566799e4895b
Martin Tsang <m4rtin.t@gmail.com>
2018-08-05 17:46:49 -0700

    Rotating ingredient

--- Assets/Script/Input/CenterIngredient.cs
+++ Assets/Script/Input/CenterIngredient.cs
@@ -11,11 +11,18 @@ public class CenterIngredient : MonoBehaviour
     [SerializeField]
     GameObject UserInput;
 
-    float m_Speed;
+    //Movement
+    float m_MovementSpeed;
     Vector3 m_StartPosition;
     Vector3 m_EndPosition;
 
+    //Rotation
+    [SerializeField]
+    float m_RotationSpeed;
+    bool m_isRotateClockwise;
+
     bool m_StartCenterIngredientAnimation;
+    bool m_StartCenterRotatationAnimation;
 
     [SerializeField]
     GameObject m_IngredientGeneratorObj;
@@ -29,29 +36,43 @@ public class CenterIngredient : MonoBehaviour
     {
         m_StartPosition = transform.position;
         m_StartCenterIngredientAnimation = false;
+        m_StartCenterRotatationAnimation = false;
+        m_isRotateClockwise = true;
         m_IngredientGenerator = m_IngredientGeneratorObj.GetComponent<IngredientGenerator>();
         SetupDelegate();
     }
 
     void SetupDelegate()
     {
-        UserInput.GetComponent<UserInput>().snapOffDelegate += StartSnapOffScreenAnimation;
+        UserInput.GetComponent<UserInput>().snapOffDelegate += StartCenterAnimation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        SnapOffScreenAnimation();
+        SnapToGhostAnimation();
+        RotateCenterAnimation();
     }
 
     #endregion
 
     #region GetterSetter
-    void StartSnapOffScreenAnimation(Direction dir)
+    void StartCenterAnimation(Direction dir)
     {
         this.m_SwipedDirection = dir;
         m_EndPosition = GetIngredient().transform.position;
-        m_StartCenterIngredientAnimation = true;
+        if (GetIngredient().GetType() == typeof(Sauce))
+        {
+            m_StartCenterRotatationAnimation = true;
+            if (dir == Direction.Left)
+            {
+                m_isRotateClockwise = false;
+            }
+        } else
+        {
+            m_StartCenterIngredientAnimation = true;
+        }
+        
     }
 
     Ingredient GetIngredient()
@@ -72,26 +93,68 @@ public class CenterIngredient : MonoBehaviour
     #endregion
 
     #region Movement
-
-    void SnapOffScreenAnimation()
+    void SnapToGhostAnimation()
     {
         if (m_StartCenterIngredientAnimation)
         {
-            m_Speed += Time.deltaTime / m_TimeToReachTarget;
-            transform.position = Vector3.Lerp(transform.position, m_EndPosition, m_Speed);
+            m_MovementSpeed += Time.deltaTime / m_TimeToReachTarget;
+            transform.position = Vector3.Lerp(transform.position, m_EndPosition, m_MovementSpeed);
             if (transform.position == m_EndPosition)
             {
                 ResetCenterAnimation();
-                transform.position = m_StartPosition;
                 UserInput.GetComponent<UserInput>().RunSwipeDelegate();
             }
         } 
     }
 
+    void RotateCenterAnimation()
+    {
+        if (m_StartCenterRotatationAnimation)
+        {
+            if (!IsAngleReached())
+                RotateCenter();
+            else { 
+                ResetCenterAnimation();
+                UserInput.GetComponent<UserInput>().RunSwipeDelegate();
+            }
+
+        }
+    }
+
+    void RotateCenter()
+    {
+       float speed = m_RotationSpeed * Time.deltaTime;
+      if (m_isRotateClockwise)
+         transform.Rotate(Vector3.back * speed);
+      else
+         transform.Rotate(Vector3.forward * speed);
+    }
+
+    bool IsAngleReached()
+    {
+        switch (this.m_SwipedDirection)
+        {
+            case Direction.Up:
+                return true;
+            case Direction.Left:
+                return transform.rotation.eulerAngles.z > 90;
+            case Direction.Right:
+                return transform.rotation.eulerAngles.z < 270 && transform.rotation.eulerAngles.z > 0;
+            case Direction.Down:
+                return transform.rotation.eulerAngles.z < 180 && transform.rotation.eulerAngles.z > 0;
+            default:
+                return false;
+        }
+    }
+
     void ResetCenterAnimation()
     {
+        transform.position = m_StartPosition;
+        transform.rotation = Quaternion.identity;
         m_StartCenterIngredientAnimation = false;
-        m_Speed = 0;
+        m_StartCenterRotatationAnimation = false;
+        m_isRotateClockwise = true;
+        m_MovementSpeed = 0;
     }
     
     #endregion
