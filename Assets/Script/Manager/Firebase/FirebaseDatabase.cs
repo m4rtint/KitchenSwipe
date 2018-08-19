c700a268f9430dd18aa18686aa282c4831fac51e
Martin Tsang <m4rtin.t@gmail.com>
2018-08-19 00:46:20 -0700

    IMporting DB and structuring it

---
+++ Assets/Script/Manager/Firebase/FirebaseDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Unity.Editor;
+
+public class FirebaseDatabase : MonoBehaviour {
+
+    public static FirebaseDatabase instance = null;
+    readonly string DB_URL = "https://kitchen-swipe.firebaseio.com/";
+
+    #region Mono
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        InitializeFirebase();
+    }
+
+    void InitializeFirebase()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(DB_URL);
+    }
+    #endregion
+}
--- Assets/Script/Manager/Firebase/FirebaseLoader.cs
+++ Assets/Script/Manager/Firebase/FirebaseLoader.cs
@@ -7,11 +7,36 @@ public class FirebaseLoader : MonoBehaviour {
     [SerializeField]
     GameObject Authentication;
 
+    [SerializeField]
+    GameObject Database;
+
     private void Awake()
     {
-        if(FirebaseAuthentication.instance == null)
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                InstantiateFirebaseModules();
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+    void InstantiateFirebaseModules()
+    {
+        if (FirebaseAuthentication.instance == null)
         {
             Instantiate(Authentication);
+        }
+
+        if (FirebaseDatabase.instance == null)
+        {
+            Instantiate(Database);
         }
     }
 
