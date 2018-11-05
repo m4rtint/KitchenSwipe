71a445a15c1b83c69b8c3579d0c03a4f68c77e1f
Martin Tsang <m4rtin.t@gmail.com>
2018-11-04 22:34:19 -0800

    Set One CLick trash

--- Assets/Script/Input/TrashInput.cs
+++ Assets/Script/Input/TrashInput.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class TrashInput : MonoBehaviour, IPointerClickHandler
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(Image))]
+public class TrashInput : MonoBehaviour
 {
     public delegate void TrashInputDelegate();
     public TrashInputDelegate animateDoubleTapDelegate;
@@ -12,34 +14,21 @@ public class TrashInput : MonoBehaviour, IPointerClickHandler
     [SerializeField]
     UserInput userInput;
 
-    readonly int doubleTap = 2;
-
-    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    private void Awake()
     {
-        if (eventData.clickCount == doubleTap && userInput.CanSwipe())
+        GetComponent<Button>().onClick.AddListener(delegate
         {
             doubleTapped();
-        }
+        });
     }
 
-    void Update()
+    public void doubleTapped()
     {
-        for (var i = 0; i < Input.touchCount; ++i)
+        if (userInput.CanSwipe())
         {
-            if (Input.GetTouch(i).phase == TouchPhase.Began)
-            {
-                if (Input.GetTouch(i).tapCount == 2)
-                {
-                    doubleTapped();
-                }
-            }
+            animateDoubleTapDelegate();
+            userInput.disableSwipe();
         }
-    }
-
-    void doubleTapped()
-    {
-        animateDoubleTapDelegate();
-        userInput.disableSwipe();
     }
 
     public void runDoubleTapDelegate()
