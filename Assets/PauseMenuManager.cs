078ce176739a9a57346fa528f53093e7291d8a37
Martin Tsang <m4rtin.t@gmail.com>
2018-12-03 22:33:57 -0800

    Set Quest in pause menu

---
+++ Assets/PauseMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PauseMenuManager : MonoBehaviour {
+
+    [SerializeField]
+    Button menuButton;
+    [SerializeField]
+    TextMeshProUGUI questsText;
+
+    private void Awake()
+    {
+        menuButton.onClick.AddListener(() => TransitionManager.instance.startMainMenuScene());
+        questsText.text = QuestManager.instance.getListOfQuestText();
+    }
+
+}
